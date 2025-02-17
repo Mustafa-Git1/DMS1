1ac94d5a87c5c1c9cffbca5647e30251bb9dc544
Mustafa-Git2 <mijklm2520@gmail.com>
2025-02-18 00:34:27 +0300

    Add Hi Mustafa

--- DMS1/Program.cs
+++ DMS1/Program.cs
@@ -4,11 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-<<<<<<< HEAD
-// Hi
-=======
-
->>>>>>> 80c3a9d85aaa92436627ca4c6da8a083198dc918
+// Hi Mustafa
 namespace DMS1
 {
     internal static class Program
